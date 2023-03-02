59f184fd8dd763620b43ca3b7d379ae0e39334c0
Lakshan Madushanka <epmadushanka@gmail.com>
2023-03-02 18:45:34 +0530

    Fix page timeout issue and change GameGenerator interface to abstract

--- app/Support/WordGenerators/RyanrkWordGenerator.php
+++ app/Support/WordGenerators/RyanrkWordGenerator.php
@@ -3,44 +3,17 @@
 namespace App\Support\WordGenerators;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
-class RyanrkWordGenerator implements WordGenerator
+class RyanrkWordGenerator extends WordGenerator
 {
     private const URL = "https://random-word.ryanrk.com/api";
-    private const ALLOWED_LETTERS = [
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'q',
-        'r',
-        's',
-        't',
-        'u',
-        'v',
-        'w',
-        'x',
-        'y',
-        'z',
-    ];
 
     public function __construct(private readonly string $language = 'en')
     {
     }
 
-    public function generate(int $length = 5): string
+    public function generateFromServer(int $length = 5): string
     {
         $word = Http::timeout(5)->withUrlParameters([
             'endPoint' => self::URL,
@@ -48,25 +21,6 @@ class RyanrkWordGenerator implements WordGenerator
             'length' => $length,
         ])->get('{+endPoint}/{language}/word/random{?length}')->json()[0];
 
-        $word = strtolower($word);
-
-        while (! $this->validateWord($word)) {
-            $this->generate(6);
-        }
-
-        return $word;
-    }
-
-    public function validateWord(string $word): bool
-    {
-        $length = strlen($word);
-
-        for ($i = 0; $i < $length; $i++) {
-            if (! in_array($word[$i], self::ALLOWED_LETTERS)) {
-                return false;
-            }
-        }
-
-        return true;
+        return strtolower($word);
     }
 }
--- app/Support/WordGenerators/WordGenerator.php
+++ app/Support/WordGenerators/WordGenerator.php
@@ -2,7 +2,60 @@
 
 namespace App\Support\WordGenerators;
 
-interface WordGenerator
+abstract class WordGenerator
 {
-    public function generate(int $length = 6): string;
+     const ALLOWED_LETTERS = [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+    ];
+
+     abstract public function generateFromServer(): string;
+
+     final public function generate(int $length = 5): string
+     {
+         $word = $this->generateFromServer($length);
+
+         while (! $this->validateWord($word)) {
+             $word =  $this->generate($length);
+         }
+
+         return $word;
+     }
+
+    public function validateWord(string $word): bool
+    {
+        $length = strlen($word);
+
+        for ($i = 0; $i < $length; $i++) {
+            if (! in_array($word[$i], self::ALLOWED_LETTERS)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
